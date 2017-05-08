de76d06159714e5083435340e2b5c26378cec484
Michael Masterson <michaelmasterson@Michaels-MacBook-Pro.local>
2017-05-08 16:50:34 -0700

    all tests passing from intersection function

---
+++ test/setIntersection_test.js
@@ -0,0 +1,21 @@
+import { expect } from 'chai'
+import setIntersection from '../src/setIntersection'
+
+describe('setIntersection()', function(){
+
+  it('should be a function', function(){
+    expect(setIntersection).to.be.a('function')
+  })
+
+  it('should return the setIntersection of a number to the nth position', function(){
+    const a = [1, 2, 3, 4]
+    const b = [2, 4, 6, 8]
+    expect(setIntersection(a, b)).to.deep.equal([2, 4])
+  })
+
+  it('should return the setIntersection of a number to the nth position', function(){
+    const a = [1, 2, 3, 4]
+    const b = [3, 5, 7, 9]
+    expect(setIntersection(a, b)).to.deep.equal([3])
+  })
+})
