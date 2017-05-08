934538b20745e91dc2c32b5cf4cd167e1de18020
Michael Masterson <michaelmasterson@Michaels-MacBook-Pro.local>
2017-05-08 16:12:27 -0700

    passing all tests for collatz function

---
+++ src/collatzConjecture.js
@@ -0,0 +1,19 @@
+export default function collatzConjecture(num){
+
+  const collNums = [num]
+
+  if (num === 1){
+    return collNums
+  } else {
+    while (num !== 1) {
+      if (num % 2 === 0) {
+        num = num / 2
+        collNums.push(num)
+      } else {
+        num = 3 * num + 1
+        collNums.push(num)
+      }
+    }
+  }
+  return collNums
+}
